25d8384177e9272cd7902b010e5decb7aeba63e5
Joshua Chu <jchutrue@gmail.com>
2017-07-29 16:15:54 -0500

    Modularized rec file io a little more

--- jagular/io.py
+++ jagular/io.py
@@ -15,6 +15,8 @@ class SpikeGadgetsRecFileReader():
         if header_size is None:
             header_size = start_byte_size
 
+        # not every recording will have neural data        
+        self.neural_data_size = 0
         self.start_byte_size = start_byte_size
         self.timestamp_size = timestamp_size
         self.bytes_per_neural_channel = bytes_per_neural_channel
@@ -28,38 +30,26 @@ class SpikeGadgetsRecFileReader():
 
         #TODO: JOSH LET'S FIX THIS!!!
         self.header_size = 1
-        ii = 0
-        # read .rec file embedded workspace and copy to a string
-        with open(filename, 'rb') as infile:
-            instr = infile.readline()
-            while(instr != b'</Configuration>\n'):
-                instr = infile.readline()
-                ii += 1
-                # infinite loop protection
-                if ii > 1000:
-                    print("Configuration info not found - check input file")
-                    break
-            config_section_size = infile.tell()
-            infile.seek(0, os.SEEK_SET)
-            xmlstring = infile.read(config_section_size)
 
-            # create xml tree from copied embedded workspace string
-            tree = ET.ElementTree(ET.fromstring(xmlstring))
-            root = tree.getroot()
-            hw_config = root.find("HardwareConfiguration")
+        # create xml tree from copied embedded workspace string
+        tree = self.get_config_info(filename)
+        root = tree.getroot()
+        hw_config = root.find("HardwareConfiguration")
 
-            # calculate packet size
-            if hw_config is None:
-                print("No hardware configuration defined!")
+        # calculate packet size
+        if hw_config is None:
+            print("No hardware configuration defined!")
+        else:
             self.neural_data_size = int(hw_config.get("numChannels"))*self.bytes_per_neural_channel
             for elements in hw_config.getchildren():
                 self.header_size += int(elements.get("numBytes"))
 
-            # every packet needs a timestamp
-            self.packet_size = self.header_size + self.timestamp_size + self.neural_data_size
+        # every packet needs a timestamp
+        self.packet_size = self.header_size + self.timestamp_size + self.neural_data_size
 
+        with open(filename, 'rb') as infile:
             # find first and last timestamps of file
-            infile.seek(config_section_size, os.SEEK_SET)
+            infile.seek(self.config_section_size, os.SEEK_SET)
             packet = infile.read(self.packet_size)
             timestamp_start = self.header_size
             # <I format - assumes that the timestamp is an uint32
@@ -69,6 +59,29 @@ class SpikeGadgetsRecFileReader():
             last_timestamp = struct.unpack('<I', packet[timestamp_start:timestamp_start + self.timestamp_size])[0]
 
         return (first_timestamp, last_timestamp, filename)
+
+    def get_config_info(self, filename):
+        import os
+        import xml.etree.ElementTree as ET
+        ii = 0
+        xmlstring = None
+
+        # read .rec file embedded workspace and copy to a string
+        with open(filename, 'rb') as infile:
+            instr = infile.readline()
+            while(instr != b'</Configuration>\n'):
+                instr = infile.readline()
+                ii += 1
+                # infinite loop protection
+                if ii > 1000:
+                    raise ValueError("Configuration info not found - check input file")
+            self.config_section_size = infile.tell()
+            infile.seek(0, os.SEEK_SET)
+            xmlstring = infile.read(self.config_section_size)
+
+        # create xml tree from copied embedded workspace string
+        xmltree = ET.ElementTree(ET.fromstring(xmlstring))
+        return xmltree
 
 
 class JagularFileMap(object):
